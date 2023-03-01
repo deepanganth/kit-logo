9cf595f946cf366b637fb80aa84e1b398448fac3
deepanganth <deepanganth@gmail.com>
2023-03-01 13:45:05 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import React, { Suspense,useRef} from 'react'
+import { Canvas, useLoader,useFrame } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+// import duck from './duck.glb'
+import { OrbitControls } from "@react-three/drei";
+import display from './kit2.glb';
+import './styles.css'
+function Object() {
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const mesh = useRef()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useFrame(() => {
+    mesh.current.rotation.y += 0.01
+  })
+
+  const gltf = useLoader(GLTFLoader, display)
+  return <primitive ref={mesh}  object={gltf.scene} position={[0, -3.5, 0]} />
+}
+
+function App() {
+  return (
+    <>
+      <Canvas >
+        <ambientLight intensity={0.5} />
+        <OrbitControls />
+        <spotLight intensity={0.8} position={[300, 300, 400]} />
+        <Suspense >{<Object/>}</Suspense>
+      </Canvas>
+      <div className='designer'>
+        <h3>Designed by</h3>
+        <h1>Deepanganth M</h1>
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
